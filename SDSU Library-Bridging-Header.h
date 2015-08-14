519acbe01993470b436347fcd77fb79f79a6816b
Tyler Rogers <t1rogers@mac.com>
2015-08-14 15:44:04 -0700

    Adding bridging header

---
+++ SDSU Library-Bridging-Header.h
@@ -0,0 +1,17 @@
+//
+//  SDSU Library Bridging Header.h
+//  SDSU Library
+//
+//  Created by Tyler Rogers on 8/14/15.
+//  Copyright (c) 2015 San Diego State University. All rights reserved.
+//
+
+#ifndef SDSU_Library_SDSU_Library_Bridging_Header_h
+#define SDSU_Library_SDSU_Library_Bridging_Header_h
+#import <SDSU Library/SDSU Library-Swift.h>
+
+#import "BasementLAViewController.h"
+#import <QuartzCore/QuartzCore.h>
+#import "ImageScrollView.h"
+
+#endif
